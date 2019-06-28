39433b095b78fb4c8473d27cf322133e7ec8fa65
dk1008 <49761725+dk1008@users.noreply.github.com>
2019-06-28 16:57:42 +0530

    Add files via upload

--- Month.java
+++ Month.java
@@ -3,73 +3,116 @@ import java.util.Scanner;
 class Month {
 	public static void main(String[] args)
 	{
-		//String s = new Scanner(System.in).nextLine();
-		//String arr[] = s.split("-");
 		System.out.println("date format should be in dd-mm-yyyy");
 		String arr[] = new Scanner(System.in).nextLine().split("-");
-		if(arr.length > 3)
-			System.out.println("It is not a valid date.");
+		try{
+			if(arr.length != 3)
+				throw new Exception();
 
-		int ch = isLeapYear(arr[2]);
-		if(ch == 0 || ch == 1)
-			display(arr[0], arr[1], ch);
-		else
-			System.out.println("Enter Valid Date-Month-Year");
+			boolean ch = isLeapYear(arr[2]);
+
+			try
+			{
+				display(arr[0], arr[1], ch);
+			}
+			catch(MonthException Me)
+			{
+				System.out.println(Me);
+			}
+			catch(DateException De)
+			{
+				System.out.println(De);
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println("Valid date format is dd-mm-yyyy");
+		}
+		
+		
 	}
 
-	public static int isLeapYear(String str)
+	public static boolean isLeapYear(String str)
 	{
 		int year = Integer.parseInt(str);
-		if(year < 0)
-			return -1;
 		
 		if(year%4 == 0)
 		{
 			if(year%100 == 0)
 			{
 				if(year%400 == 0)
-					return 1;
+					return true;
 				else
-					return 0;
+					return false;
 			}
 			else
-				return 1;
+				return true;
 		}
 		else
 		{
-			return 0;
+			return false;
 		}
 	}
 
-	public static void display(String str_d, String str_m , int ch)
+	public static void display(String str_d, String str_m , boolean ch) throws MonthException,DateException
 	{
 		String[] arr = {"January", "February", "March", "April", "May", "June", "July", "August", "Sepetember", "October", "November", "December" };
 		int[] date = {31,28,31,30,31,30,31,31,30,31,30,31};
 
 		int curr_date = Integer.parseInt(str_d);
-		//System.out.println(curr_date);
 		int month = Integer.parseInt(str_m);
-		//System.out.println(month);
-		if(month>0 && month < 12 )
-		{
-			if(ch == 0)
+
+			if(month>0 && month < 12 )
 			{
-				if(curr_date <= date[month-1])
-					System.out.println(arr[month-1]+" has "+date[month-1]+" Days.");
+				if(ch)
+				{
+					if((month==2)&&(curr_date <= (date[1]+1)))
+						System.out.println(arr[month-1]+" has "+( date[month-1]+1)+" Days.");
+					else if(curr_date <= date[month-1])
+						System.out.println(arr[month-1]+" has "+date[month-1]+" Days.");
+					else
+						throw new DateException("Entered Date is Wrong");	
+				}
 				else
-					System.out.println("Enter a valid Date.");						
+				{
+					if(curr_date <= date[month-1])
+						System.out.println(arr[month-1]+" has "+date[month-1]+" Days.");
+					else
+						throw new DateException("Entered Date is Wrong");
+				}
 			}
 			else
-			{
-				if((month==2)&&(curr_date <= (date[1]+1)))
-					System.out.println(arr[month-1]+" has "+( date[month-1]+1)+" Days.");
-				else if(curr_date <= date[month-1])
-					System.out.println(arr[month-1]+" has "+date[month-1]+" Days.");
-				else
-					System.out.println("Enter a valid Date.");	
-			}
-		}
-		else
-			System.out.println("Entered Month is Wrong");
+				throw new MonthException("Entered Month is Wrong");	
 	}	
+}
+
+
+class MonthException extends Exception
+{
+	String str ="" ;
+	
+	MonthException(String st1)
+	{
+		str = st1;
+	}
+
+	public String toString()
+	{
+		return (str);
+	}
+}
+
+class DateException extends Exception
+{
+	String str ="" ;
+	
+	DateException(String st1)
+	{
+		str = st1;
+	}
+
+	public String toString()
+	{
+		return (str);
+	}
 }
